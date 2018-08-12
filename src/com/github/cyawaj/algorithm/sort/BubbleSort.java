d946b634058ccd5a66f125dbbdabd8d46a4ba644
jawayc <jawayc@163.com>
2018-08-12 15:32:54 +0800

    快速排序、归并排序、插入排序和选择排序

--- src/com/github/cyawaj/algorithm/sort/BubbleSort.java
+++ src/com/github/cyawaj/algorithm/sort/BubbleSort.java
@@ -10,8 +10,14 @@ import java.util.Random;
  * @date 2018-08-09
  */
 public class BubbleSort {
+	/**
+	 * 较大的数往后(最前面的不变)
+	 * 
+	 * @param array
+	 */
 	public static void sort(int[] array) {
 		for (int i = 1; i < array.length; i++) {
+			// 比较轮数 array.length - 1
 			for (int j = 0; j < array.length - i; j++) {
 				if (array[j] > array[j + 1]) {
 					// 1.中间变量交换
@@ -27,6 +33,49 @@ public class BubbleSort {
 		}
 	}
 
+	/**
+	 * 较小的数往前(最后面的不变)
+	 * 
+	 * @param array
+	 */
+	public static void sort2(int[] array) {
+		int temp;
+		for (int i = 1; i < array.length; i++) {
+			// 比较轮数 array.length - 1
+			for (int j = array.length - 1; j >= i; j--) {
+				if (array[j] < array[j - 1]) {
+					temp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * 没有比较过，说明已经排好序
+	 * 
+	 * @param array
+	 */
+	public static void sort3(int[] array) {
+		int temp;
+		for (int i = 1; i < array.length; i++) {
+			// 比较轮数 array.length - 1
+			boolean flag = false;
+			for (int j = array.length - 1; j >= i; j--) {
+				if (array[j] < array[j - 1]) {
+					temp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temp;
+					flag = true;
+				}
+			}
+			if (!flag) {
+				break;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		int length = 100;
 		int[] array = new int[length];
@@ -35,7 +84,7 @@ public class BubbleSort {
 			array[i] = random.nextInt(100);
 		}
 		System.out.println("原本数组:" + Arrays.toString(array));
-		sort(array);
+		sort2(array);
 		System.out.println("排序后的数组:" + Arrays.toString(array));
 	}
 }
